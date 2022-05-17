1af3a859d72b82b054c779777c8132cade41f035
ThomasGrc <47837271+ThomasGrc@users.noreply.github.com>
2022-05-17 14:42:39 +0000

    Deploying to gh_pages from @ WhisprStudio/whispr-website-next@bc138ff098fa5f699aa8539a8c302df9a3e13cbe 🚀

--- _next/static/a8_Nujsota5mCKuiaYmcj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/750-17076975fbc51334.js","static/css/7690dc5863afed6c.css","static/chunks/pages/index-9bc6ee8668876605.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
