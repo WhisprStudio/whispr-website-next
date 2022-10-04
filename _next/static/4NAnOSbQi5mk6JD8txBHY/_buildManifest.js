b67ad1bdcee0e39ae2f213c734bb6304a0dbcd0b
Nero-F <fahad.assoumani@epitech.eu>
2022-10-04 20:05:18 +0000

    Deploying to gh_pages from @ WhisprStudio/whispr-website-next@4eb4c01a2ce04704d584d0d01f7150e725a54151 🚀

--- _next/static/4NAnOSbQi5mk6JD8txBHY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/750-17076975fbc51334.js","static/css/4defbbaf52d306ae.css","static/chunks/pages/index-7d803f080cbb0a97.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
