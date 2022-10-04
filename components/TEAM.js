4bf775fdaeb75498da1b9a63827922f863acc3fb
Fahad Assoumani <fahad.assoumani@epitech.eu>
2022-10-04 21:50:13 +0200

    update: team.css to team.module.css

--- components/TEAM.js
+++ components/TEAM.js
@@ -1,4 +1,4 @@
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Team.module.css"
 import { Avatar } from "@mui/material"
 import { useGlobalState } from '../pages';
 
@@ -6,37 +6,37 @@ const teamArr = [
     {
         src: "/photos/Fahad.jpg",
         name: "Fahad Assoumani",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
     {
         src: "/photos/Thomas.jpg",
         name: "Thomas Garcia",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
     {
         src: "/photos/Paul.jpg",
         name: "Paul Erny",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
     {
         src: "/photos/Lucas.jpg",
         name: "Lucas Cherloneix",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
     {
         src: "/photos/Victor.jpg",
         name: "Victor Maurin",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
     {
         src: "/photos/Hugo.jpg",
         name: "Hugo Lalet",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
     {
         src: "/photos/Antonin.jpg",
         name: "Antonin Keuck",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+        desc: "Lorem ipsum dolor sit amet",
     },
 ]
 
@@ -47,17 +47,17 @@ export default function Team() {
         <div className={styles.team}>
             {
                 teamArr.map(member => { return (
-                    <>
+                    <div className={styles.member}>
                         <Avatar
-                            alt="Remy Sharp"
+                            alt={member.name}
                             src={member.src}
-                            sx={{ width: 200, height: 200 }}
+                            sx={{ width: 100, height: 100 }}
                         />
-                        <h2 className={darkMode ? "dark" : ""}>{member.name}</h2>
+                        <h3 className={darkMode ? "dark" : ""}>{member.name}</h3>
                         <p className={darkMode ? "dark" : ""}>
                             {member.desc}
                         </p>
-                    </>
+                    </div>
                 );
                 })
             }
