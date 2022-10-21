3a86d16435754f2d9380026c96e360b6eaa9c5b4
ThomasGrc <47837271+ThomasGrc@users.noreply.github.com>
2022-10-21 22:50:06 +0200

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -6,6 +6,7 @@ export function Footer() {
     const [darkMode, setDarkMode] = useGlobalState('darkMode');
 
     return <div className={`${styles.footerContainer} ${darkMode ? styles.dark : " "}`}>
+        <h2>Do you want to contact us ?</h2>
         <p className={darkMode ? "dark" : " "}>whispr.<span>studio</span>@gmail.com &copy;</p>
     </div>
 }
